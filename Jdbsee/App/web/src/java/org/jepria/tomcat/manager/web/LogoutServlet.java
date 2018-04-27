282e17e3ef0ded5d3f6e21868de9ccec6fd4836b
remitot <call9096463086@gmail.com>
2018-04-27 16:53:06 +0300

    [*] fix logout

--- Jdbsee/App/web/src/java/org/jepria/tomcat/manager/web/LogoutServlet.java
+++ Jdbsee/App/web/src/java/org/jepria/tomcat/manager/web/LogoutServlet.java
@@ -13,6 +13,7 @@ public class LogoutServlet extends HttpServlet {
   
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    req.logout();
     req.getSession().invalidate();
   }
 
