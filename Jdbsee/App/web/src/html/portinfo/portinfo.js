53443fee946233e76e7c97df8b6db402bf0e4de7
Romanov Alexey <call9096463086@gmail.com>
2019-01-16 23:05:17 +0300

    feat(web/portinfo): added portinfo GUI

---
+++ Jdbsee/App/web/src/html/portinfo/portinfo.js
@@ -0,0 +1,67 @@
+function getJsonListItems(jsonResponse) {
+  return jsonResponse // the response itself is a JSON array of port infos
+}
+
+function getApiListUrl() {
+  return "api/portinfo/list";
+}
+
+function getApiModUrl() {
+  // unsupported
+}
+
+function createHeader() {
+  var row = document.createElement("div");
+  row.classList.add("header");
+  
+  div = document.createElement("div");
+  div.classList.add("flexColumns");
+  
+  cell = createCell(div, "column-type");
+  label = document.createElement("label");
+  label.innerHTML = "Тип"; // NON-NLS
+  cell.appendChild(label);
+  
+  cell = createCell(div, "column-port");
+  label = document.createElement("label");
+  label.innerHTML = "Порт"; // NON-NLS
+  cell.appendChild(label);
+  
+  row.appendChild(div);
+  
+  return row;
+}
+
+var tabindex0 = 1;
+
+function createRow(listItem) {
+  row = document.createElement("div");
+  row.classList.add("row");
+  row.setAttribute("item-location", listItem.location);
+  
+  div = document.createElement("div");
+  div.classList.add("flexColumns");
+  
+  cell = createCell(div, "column-type");
+  cell.classList.add("cell-field");
+  field = addField(cell, "type", listItem.type, null);
+  field.setAttribute("value0", listItem.type);
+  field.tabIndex = tabindex0++;
+  
+  cell = createCell(div, "column-port");
+  cell.classList.add("cell-field");
+  field = addField(cell, "port", listItem.port, null);
+  field.setAttribute("value0", listItem.port);
+  field.tabIndex = tabindex0++;
+  
+  row.appendChild(div);
+  
+  
+  return row;
+}
+
+function createRowCreate() {
+  // unsupported
+}
+
+
---
+++ Jdbsee/App/web/src/html/portinfo/portinfo.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Серверные порты — Tomcat manager</title> <!-- NON-NLS -->
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"> 
+    
+    <link rel="stylesheet" href="jtm.css">
+    <link rel="stylesheet" href="table.css">
+    <link rel="stylesheet" href="portinfo/portinfo.css">
+    <script type="text/javascript" src="table.js"></script>
+    <script type="text/javascript" src="portinfo/portinfo.js"></script>
+    
+  </head>
+  
+  <body onload="reload()">
+    <div id="table" style="width:100%"></div>
+    
+    <!-- after main content elements: -->
+    <div id="jdbcStatusBar" class="statusBar statusBar-none"></div>
+    
+    <%@ include file="../login.fragment.html" %>    
+    
+  </body>
+</html>
