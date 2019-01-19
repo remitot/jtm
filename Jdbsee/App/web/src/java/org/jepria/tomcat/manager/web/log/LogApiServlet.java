7a500c7071a724ebc4d25d63c2636c6920990057
Romanov Alexey <call9096463086@gmail.com>
2019-01-19 21:16:57 +0300

    feat(web/log): getting log content

--- Jdbsee/App/web/src/java/org/jepria/tomcat/manager/web/log/LogApiServlet.java
+++ Jdbsee/App/web/src/java/org/jepria/tomcat/manager/web/log/LogApiServlet.java
@@ -1,8 +1,10 @@
 package org.jepria.tomcat.manager.web.log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,6 +12,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -31,41 +36,120 @@ public class LogApiServlet extends HttpServlet {
     String path = req.getPathInfo();
     
     if ("/list".equals(path)) {
+      list(req, resp);
+      return;
       
-      // the content type is defined for the entire method
-      resp.setContentType("application/json; charset=UTF-8");
+    } else if (path == null || "/".equals(path)) {
       
-      try {
-        
-        Environment environment = EnvironmentFactory.get(req);
- 
-        File logsDirectory = environment.getLogsDirectory();
-        
-        List<LogDto> logs = getLogs(logsDirectory);
-
-        Map<String, Object> responseJsonMap = new HashMap<>();
-        responseJsonMap.put("_list", logs);
-        
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        gson.toJson(responseJsonMap, new PrintStream(resp.getOutputStream()));
-        
-        resp.setStatus(HttpServletResponse.SC_OK);
-        resp.flushBuffer();
-        return;
-        
-      } catch (Throwable e) {
-        e.printStackTrace();
-
-        // response body must either be empty or match the declared content type
-        resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        resp.flushBuffer();
-        return;
+      // get log by filename
+      String queryString = req.getQueryString();
+      if (queryString != null) {
+        Matcher m = Pattern.compile("filename\\=([^&/\\\\]+)").matcher(queryString);
+        if (m.matches()) {
+          String filename = m.group(1);
+          fileContents(req, resp, filename);
+          return;
+          
+        }
       }
-
+      
+      resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+      resp.flushBuffer();
+      return;
+      
     } else {
       
       // TODO set content type for the error case?
       resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+      resp.flushBuffer();
+      return;
+    }
+  }
+  
+  private static void list(HttpServletRequest req, HttpServletResponse resp) 
+      throws IOException {
+    
+    // the content type is defined for the entire method
+    resp.setContentType("application/json; charset=UTF-8");
+    
+    try {
+      
+      Environment environment = EnvironmentFactory.get(req);
+
+      File logsDirectory = environment.getLogsDirectory();
+      
+      List<LogDto> logs = getLogs(logsDirectory);
+
+      Map<String, Object> responseJsonMap = new HashMap<>();
+      responseJsonMap.put("_list", logs);
+      
+      Gson gson = new GsonBuilder().setPrettyPrinting().create();
+      gson.toJson(responseJsonMap, new PrintStream(resp.getOutputStream()));
+      
+      resp.setStatus(HttpServletResponse.SC_OK);
+      resp.flushBuffer();
+      return;
+      
+    } catch (Throwable e) {
+      e.printStackTrace();
+
+      // response body must either be empty or match the declared content type
+      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      resp.flushBuffer();
+      return;
+    }
+  }
+  
+  // TODO this value is assumed. But how to determine it? 
+  private static final String LOG_FILE_READ_ENCODING = "UTF-8";
+  
+  private static final String LOG_FILE_RESPONSE_WRITE_ENCODING = "UTF-8";
+  
+  private static void fileContents(HttpServletRequest req, HttpServletResponse resp,
+      String filename) throws IOException {
+    
+    // the content type is defined for the entire method
+    resp.setContentType("text/plain; charset=UTF-8");
+    
+    try {
+      
+      Environment environment = EnvironmentFactory.get(req);
+
+      File logsDirectory = environment.getLogsDirectory();
+      
+      Path logFile = logsDirectory.toPath().resolve(filename);
+      
+      // output encoding
+      resp.setCharacterEncoding(LOG_FILE_RESPONSE_WRITE_ENCODING);
+      
+      try (Scanner sc = new Scanner(logFile.toFile(), LOG_FILE_READ_ENCODING)) {
+        while (sc.hasNextLine()) {
+          resp.getWriter().println(sc.nextLine());
+        }
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+        
+        resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        resp.flushBuffer();
+        return;
+      }
+      
+      /*
+      // XXX this solution is for binary file download (no encoding specified):
+      try (OutputStream out = resp.getOutputStream()) {
+        Files.copy(logFile, out);
+      }
+       */
+      
+      resp.setStatus(HttpServletResponse.SC_OK);
+      resp.flushBuffer();
+      return;
+      
+    } catch (Throwable e) {
+      e.printStackTrace();
+
+      // response body must either be empty or match the declared content type
+      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
       resp.flushBuffer();
       return;
     }
