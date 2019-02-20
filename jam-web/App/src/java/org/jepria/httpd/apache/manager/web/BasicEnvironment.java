fc59a48de47f78eb650b1c3d25a016234a94acef
Romanov Alexey <call9096463086@gmail.com>
2019-02-20 22:27:24 +0300

    feat(jam-web): dynamic workers.properties initialization; properties

--- jam-web/App/src/java/org/jepria/httpd/apache/manager/web/BasicEnvironment.java
+++ jam-web/App/src/java/org/jepria/httpd/apache/manager/web/BasicEnvironment.java
@@ -4,8 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,30 +21,61 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class BasicEnvironment implements Environment {
   
-  private final File mod_jk_conf;
-  private final File workers_properties;
+  private final Path mod_jk_conf;
+  private Path workers_properties;// initialized dynamically from mod_jk.conf
   
   private final EnvironmentPropertyFactory envPropertyFactory;
   
   public BasicEnvironment(HttpServletRequest request) {
     envPropertyFactory = new EnvironmentPropertyFactory(new File(request.getServletContext().getRealPath("/WEB-INF/app-conf-default.properties")));
     
-    final String confDirEnv = getProperty("org.jepria.httpd.apache.manager.web.conf.dir");
+    final String confDirEnv = getProperty("org.jepria.httpd.apache.manager.web.apacheConf");
     if (confDirEnv == null) {
       throw new RuntimeException("Misconfiguration exception: "
-          + " mandatory configuration property \"org.jepria.httpd.apache.manager.web.conf.dir\" is not defined");
+          + "mandatory configuration property \"org.jepria.httpd.apache.manager.web.apacheConf\" is not defined");
     }
     
-    final File confDir = new File(confDirEnv);
+    mod_jk_conf = Paths.get(confDirEnv).resolve("jk").resolve("mod_jk.conf");
+    if (!Files.isRegularFile(mod_jk_conf)) {
+      throw new RuntimeException("Misconfiguration exception: [" + mod_jk_conf + "] is not a file");
+    }
     
-    mod_jk_conf = confDir.toPath().resolve("jk").resolve("mod_jk.conf").toFile();
-    workers_properties = confDir.toPath().resolve("jk").resolve("workers.properties").toFile();
+    
+    // init workers.properties
+    workers_properties = null;
+    try (Scanner sc = new Scanner(mod_jk_conf)) {
+      final Pattern p = Pattern.compile("\\s*JkWorkersFile\\s+\"(.+)\"\\s*");
+      while (sc.hasNext()) {
+        String line = sc.nextLine();
+        Matcher m = p.matcher(line);
+        if (m.matches()) {
+          final String workersFileDef = m.group(1);
+          Path workersFile = Paths.get(workersFileDef);// normally begins with 'conf/'
+          Path workersFileRel = workersFile.getName(0).relativize(workersFile);
+          workers_properties = Paths.get(confDirEnv).resolve(workersFileRel);
+          if (!Files.isRegularFile(workers_properties)) {
+            throw new RuntimeException("Misconfiguration exception: "
+                + "the directive [" + line + "] in the file [" + mod_jk_conf + "] "
+                    + "does not represent a file");
+          }
+          break;
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    
+    if (workers_properties == null) {
+      throw new RuntimeException("Misconfiguration exception: "
+          + "the file [" + mod_jk_conf + "] contains no 'JkWorkersFile' directive");
+    }
+    
   }
   
   @Override
   public OutputStream getMod_jk_confOutputStream() {
     try {
-      return new FileOutputStream(mod_jk_conf);
+      return new FileOutputStream(mod_jk_conf.toFile());
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);//TODO?
     }
@@ -46,7 +84,7 @@ public class BasicEnvironment implements Environment {
   @Override
   public InputStream getMod_jk_confInputStream() {
     try {
-      return new FileInputStream(mod_jk_conf);
+      return new FileInputStream(mod_jk_conf.toFile());
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);//TODO?
     }
@@ -55,7 +93,7 @@ public class BasicEnvironment implements Environment {
   @Override
   public OutputStream getWorkers_propertiesOutputStream() {
     try {
-      return new FileOutputStream(workers_properties);
+      return new FileOutputStream(workers_properties.toFile());
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);//TODO?
     }
@@ -64,7 +102,7 @@ public class BasicEnvironment implements Environment {
   @Override
   public InputStream getWorkers_propertiesInputStream() {
     try {
-      return new FileInputStream(workers_properties);
+      return new FileInputStream(workers_properties.toFile());
     } catch (FileNotFoundException e) {
       throw new RuntimeException(e);//TODO?
     }
