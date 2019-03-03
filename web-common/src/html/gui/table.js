c9d1ec685cbc098f1f1fa4b43a477894128570ac
Romanov Alexey <call9096463086@gmail.com>
2019-03-03 12:41:33 +0300

    fix(web-common): checkbox onclick handling

--- web-common/src/html/gui/table.js
+++ web-common/src/html/gui/table.js
@@ -238,10 +238,15 @@ function addTableScript(table) {
     });
     
     
-    checkbox.onclick = function(event) {
-      onCheckboxInput(checkbox);
-      checkModifications();
-    };
+    checkbox.onclick = function(checkbox) {// javascript doesn't use block scope for variables
+      return function(event) {
+      	// JS note: the handler is being invoked twice per single click 
+	  	  // (first for onclick:event.target=SPAN, second for onclick:event.target=INPUT)
+	  	  if (event.target.tagName.toLowerCase() == "input") {
+          onCheckboxInput(checkbox);
+        }
+      }
+    }(checkbox);
   }
   
   addRowButtonCreateScript(table);
@@ -320,31 +325,30 @@ function onCheckboxInput(checkbox) {
   // TODO bad direct access
   var input = checkbox.getElementsByTagName("input")[0];
   
-  // this will be SPAN, then INPUT on a single click
-  if (input.tagName.toLowerCase() == "input") {
-    if (input.checked && checkbox.getAttribute("value-original") == "true" 
-        || !input.checked && checkbox.getAttribute("value-original") == "false") {
-      // affect both input (to get the modified fields selected by input.modified) 
-      // and label (to graphically display the field modification state)
-      input.parentElement.classList.remove("modified");
-      input.classList.remove("modified");
-    } else {
-      // affect both input (to get the modified fields selected by input.modified) 
-      // and label (to graphically display the field modification state)
-      input.parentElement.classList.add("modified");
-      input.classList.add("modified");
-    }
-    
-    if (!input.checked) {
-      //TODO resolve the relative path:
-      input.parentElement.parentElement.parentElement.parentElement.parentElement.classList.add("inactive");
-      input.parentElement.title = "Запись неактивна"; // NON-NLS
-    } else {
-      //TODO resolve the relative path:
-      input.parentElement.parentElement.parentElement.parentElement.parentElement.classList.remove("inactive");
-      input.parentElement.title = "Запись активна"; // NON-NLS
-    }
+  if (input.checked && checkbox.getAttribute("value-original") == "true" 
+      || !input.checked && checkbox.getAttribute("value-original") == "false") {
+    // affect both input (to get the modified fields selected by input.modified) 
+    // and label (to graphically display the field modification state)
+    checkbox.classList.remove("modified");
+    input.classList.remove("modified");
+  } else {
+    // affect both input (to get the modified fields selected by input.modified) 
+    // and label (to graphically display the field modification state)
+    checkbox.classList.add("modified");
+    input.classList.add("modified");
   }
+  
+  if (!input.checked) {
+    //TODO resolve the relative path:
+    checkbox.parentElement.parentElement.parentElement.parentElement.classList.add("inactive");
+    checkbox.title = "Запись неактивна"; // NON-NLS
+  } else {
+    //TODO resolve the relative path:
+    checkbox.parentElement.parentElement.parentElement.parentElement.classList.remove("inactive");
+    checkbox.title = "Запись активна"; // NON-NLS
+  }
+  
+  checkModifications();
 }
 
 function addField(cell, name, value, placeholder) {
@@ -613,11 +617,6 @@ function addRowCreateScript(rowCreate) {
 	for (var i = 0; i < fields.length; i++) {
 	  onFieldInput(fields[i]);
 	}
-	
-	var checkboxes = rowCreate.querySelectorAll(".checkbox");
-  for (var i = 0; i < checkboxes.length; i++) {
-    onCheckboxInput(checkboxes[i]);
-  }
 	
 	// focus on the first text input field
 	rowCreate.querySelectorAll(".cell input[type='text']")[0].focus();
