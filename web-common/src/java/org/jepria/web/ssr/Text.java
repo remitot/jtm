ca551ca7e7eb5de3ee4f2096955ba1132efe5721
remitot <call9096463086@gmail.com>
2019-04-30 13:56:22 +0300

    feat(web-common): supported applicational text resources

--- web-common/src/java/org/jepria/web/ssr/Text.java
+++ web-common/src/java/org/jepria/web/ssr/Text.java
@@ -9,19 +9,35 @@ public interface Text {
   String getString(String key);
   
   public static final String DEFAULT_LANG = "ru";
-  
-  public static Text fromRequest(HttpServletRequest request) {
+
+  /**
+   * 
+   * @param request
+   * @param bundleBaseName applicational resource bundle base name
+   * @return
+   */
+  public static Text get(HttpServletRequest request, String bundleBaseName) {
     return new Text() {
-      
+
       private final String requestLang = request.getParameter("lang");
-      
+
       private final String localeCode = requestLang == null ? DEFAULT_LANG : requestLang;
-      
-      private final ResourceBundle bundle = ResourceBundle.getBundle("text/bundle", new Locale(localeCode));
-      
+
+      private final ResourceBundle bundleApp;
+      private final ResourceBundle bundleCommon;
+
+      {
+        bundleApp = ResourceBundle.getBundle(bundleBaseName, new Locale(localeCode));
+        bundleCommon = ResourceBundle.getBundle("text/org_jepria_web_ssr_Text", new Locale(localeCode));
+      }
+
       @Override
       public String getString(String key) {
-        return bundle.getString(key);
+        if (bundleApp.containsKey(key)) {
+          return bundleApp.getString(key);
+        } else {
+          return bundleCommon.getString(key);
+        }
       }
     };
   }
