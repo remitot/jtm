d1fb9c08f90a71b0c0a38c6be8ad60cc6946c88d
remitot <call9096463086@gmail.com>
2019-05-15 19:37:43 +0300

    chore

--- jtm-web/src/java/org/jepria/tomcat/manager/web/port/dto/PortDto.java
+++ jtm-web/src/java/org/jepria/tomcat/manager/web/port/dto/PortDto.java
@@ -2,7 +2,6 @@ package org.jepria.tomcat.manager.web.port.dto;
 
 import java.util.HashMap;
 
-// TODO remove the class?
 public class PortDto extends HashMap<String, String> {
   private static final long serialVersionUID = 1L;
 }
