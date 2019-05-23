5f51d18c3dc3e8ca0c691a4ce5d74cda7c075f51
remitot <call9096463086@gmail.com>
2019-05-23 17:40:49 +0300

    feat(jam-web/jk): detail form

--- jam-web/src/java/org/jepria/httpd/apache/manager/web/jk/JkDetails.java
+++ jam-web/src/java/org/jepria/httpd/apache/manager/web/jk/JkDetails.java
@@ -16,61 +16,184 @@ public class JkDetails extends El {
     addClass("details");
     setAttribute("id", "table");
     
-    addStyle("css/table.css");
-    addStyle("css/jk/details.css");
+    addStyle("css/table.css");// TODO refactor: table.css must be required for Table.java descendants only
+    addStyle("css/jk/jk.css");
   }
   
   public void load(JkMountDto mount, WorkerDto worker) {
-    if (mount == null) {
-      appendChild(new El("div").setInnerHTML("mount is null!"));
-    } else {
-      {
-        FieldTextLabel label = new FieldTextLabel();
-        label.setInnerHTML("active", true);// TODO NON-NLS
-        label.addClass("field-label");
-        
-        boolean value = !"false".equals(mount.map.get("active"));
-        FieldCheckBox field = new FieldCheckBox(
-            "active", value, value, false, null);
-        
-        El row = new El("div");
-        row.addClass("row");
-        
-        row.appendChild(new El("div").addClass("cell field-label").appendChild(Fields.wrapCellPad(label)));
-        row.appendChild(new El("div").addClass("cell field-active").appendChild(Fields.wrapCellPad(field)));
-        
-        appendChild(row);
+    
+    // mount fields
+    {
+      FieldTextLabel label = new FieldTextLabel();
+      label.setInnerHTML("active", true);// TODO NON-NLS
+      label.addClass("field-label");
+      
+      boolean value;
+      if (mount != null) {
+        value = !"false".equals(mount.map.get("active"));
+      } else {
+        value = true;
       }
-      {
-        FieldTextLabel label = new FieldTextLabel();
-        label.setInnerHTML("application", true);// TODO NON-NLS
-        label.addClass("field-label");
-        
-        String value = mount.map.get("application");
-        FieldTextInput field = new FieldTextInput(
-            "application", value, value, "application", false, null);// TODO NON-NLS placeholder
-        
-        El row = new El("div");
-        row.addClass("row");
-        
-        row.appendChild(new El("div").addClass("cell field-label").appendChild(Fields.wrapCellPad(label)));
-        row.appendChild(new El("div").addClass("cell").appendChild(Fields.wrapCellPad(field)));
-        
-        appendChild(row);
+      FieldCheckBox field = new FieldCheckBox(
+          "active", value, value, false, null);
+      
+      El cellDelete = new El("div").addClass("cell column-delete cell_field");// TODO stopped here: remove column-delete from non-table
+      addFieldDelete(cellDelete, "del", "und");
+      
+          
+      El row = new El("div");
+      row.addClass("row");
+      
+      row.appendChild(new El("div").addClass("cell cell_field-label").appendChild(Fields.wrapCellPad(label)));
+      row.appendChild(new El("div").addClass("cell cell_field").appendChild(Fields.wrapCellPad(field)));
+      row.appendChild(cellDelete);
+      
+      appendChild(row);
+    }
+    {
+      FieldTextLabel label = new FieldTextLabel();
+      label.setInnerHTML("application", true);// TODO NON-NLS
+      label.addClass("field-label");
+      
+      String value;
+      if (mount != null) {
+        value = mount.map.get("application");
+      } else {
+        value = null;
       }
+      FieldTextInput field = new FieldTextInput(
+          "application", value, value, "Application", false, null);// TODO NON-NLS placeholder
+      
+      El row = new El("div");
+      row.addClass("row");
+      
+      row.appendChild(new El("div").addClass("cell cell_field-label").appendChild(Fields.wrapCellPad(label)));
+      row.appendChild(new El("div").addClass("cell cell_field").appendChild(Fields.wrapCellPad(field)));
+      
+      appendChild(row);
     }
-    if (worker == null) {
-      appendChild(new El("div").setInnerHTML("worker is null!"));
-    } else {
-      appendChild(new El("div").appendChild(new FieldTextLabel("name:"))// TODO NON-NLS
-          .appendChild(new FieldTextLabel(worker.map.get("name"))));
-      appendChild(new El("div").appendChild(new FieldTextLabel("host:"))// TODO NON-NLS
-          .appendChild(new FieldTextLabel(worker.map.get("host"))));
-      appendChild(new El("div").appendChild(new FieldTextLabel("type:"))// TODO NON-NLS
-          .appendChild(new FieldTextLabel(worker.map.get("type"))));
-      appendChild(new El("div").appendChild(new FieldTextLabel("port:"))// TODO NON-NLS
-          .appendChild(new FieldTextLabel(worker.map.get("port"))));
+    
+    
+    // worker fields
+    {
+      FieldTextLabel label = new FieldTextLabel();
+      label.setInnerHTML("worker name", true);// TODO NON-NLS
+      label.addClass("field-label");
+      
+      String value;
+      if (worker != null) {
+        value = worker.map.get("name");
+      } else {
+        value = null;
+      }
+      FieldTextInput field = new FieldTextInput(
+          "worker-name", value, value, "worker-tomcat-8080", false, null);// TODO NON-NLS placeholder
+      
+      El row = new El("div");
+      row.addClass("row");
+      
+      row.appendChild(new El("div").addClass("cell cell_field-label").appendChild(Fields.wrapCellPad(label)));
+      row.appendChild(new El("div").addClass("cell cell_field").appendChild(Fields.wrapCellPad(field)));
+      
+      appendChild(row);
     }
+    {
+      FieldTextLabel label = new FieldTextLabel();
+      label.setInnerHTML("worker host", true);// TODO NON-NLS
+      label.addClass("field-label");
+      
+      String value;
+      if (worker != null) {
+        value = worker.map.get("host");
+      } else {
+        value = null;
+      }
+      FieldTextInput field = new FieldTextInput(
+          "worker-host", value, value, "server.com", false, null);// TODO NON-NLS placeholder
+      
+      El row = new El("div");
+      row.addClass("row");
+      
+      row.appendChild(new El("div").addClass("cell cell_field-label").appendChild(Fields.wrapCellPad(label)));
+      row.appendChild(new El("div").addClass("cell cell_field").appendChild(Fields.wrapCellPad(field)));
+      
+      appendChild(row);
+    }
+    {
+      FieldTextLabel label = new FieldTextLabel();
+      label.setInnerHTML("worker protocol", true);// TODO NON-NLS
+      label.addClass("field-label");
+      
+      String value;
+      if (worker != null) {
+        value = worker.map.get("type");
+      } else {
+        value = null;
+      }
+      FieldTextInput field = new FieldTextInput(
+          "worker-type", value, value, "ajp13", false, null);// TODO NON-NLS placeholder
+      
+      El row = new El("div");
+      row.addClass("row");
+      
+      row.appendChild(new El("div").addClass("cell cell_field-label").appendChild(Fields.wrapCellPad(label)));
+      row.appendChild(new El("div").addClass("cell cell_field").appendChild(Fields.wrapCellPad(field)));
+      
+      appendChild(row);
+    }
+    {
+      FieldTextLabel label = new FieldTextLabel();
+      label.setInnerHTML("worker port", true);// TODO NON-NLS
+      label.addClass("field-label");
+      
+      String value;
+      if (worker != null) {
+        value = worker.map.get("port");
+      } else {
+        value = null;
+      }
+      FieldTextInput field = new FieldTextInput(
+          "worker-port", value, value, "8009", false, null);// TODO NON-NLS placeholder
+      
+      El row = new El("div");
+      row.addClass("row");
+      
+      row.appendChild(new El("div").addClass("cell cell_field-label").appendChild(Fields.wrapCellPad(label)));
+      row.appendChild(new El("div").addClass("cell cell_field").appendChild(Fields.wrapCellPad(field)));
+      
+      appendChild(row);
+    }
+  }
+  
+  protected El addFieldDelete(El cell, String titleDelete, String titleUndelete) {
+
+    El field = new El("div");
+    
+    El buttonDelete = new El("input");
+    buttonDelete.classList.add("button-delete");
+    buttonDelete.classList.add("button-delete_delete");
+    buttonDelete.setAttribute("type", "image");
+    buttonDelete.setAttribute("src", "img/delete.png");
+    if (titleDelete != null) {
+      buttonDelete.setAttribute("title", titleDelete);
+    }
+    
+    El buttonUndelete = new El("input");
+    buttonUndelete.classList.add("button-delete");
+    buttonUndelete.classList.add("button-delete_undelete");
+    buttonUndelete.setAttribute("type", "image");
+    buttonUndelete.setAttribute("src", "img/undelete.png");
+    if (titleUndelete != null) {
+      buttonUndelete.setAttribute("title", titleUndelete);
+    }
+    
+    field.appendChild(buttonDelete);
+    field.appendChild(buttonUndelete);
+    
+    El wrapper = Fields.wrapCellPad(field);  
+    cell.appendChild(wrapper);
+    
+    return field;
   }
 }
 
