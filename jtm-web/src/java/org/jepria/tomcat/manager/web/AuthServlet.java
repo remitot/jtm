e1d3fe922b2f4d997b7f14f3afe25e830cc63681
remitot <call9096463086@gmail.com>
2019-07-02 18:45:00 +0300

    chore(jtm-web): java comments

--- jtm-web/src/java/org/jepria/tomcat/manager/web/AuthServlet.java
+++ jtm-web/src/java/org/jepria/tomcat/manager/web/AuthServlet.java
@@ -2,6 +2,9 @@ package org.jepria.tomcat.manager.web;
 
 import org.jepria.web.auth.AuthServletBase;
 
+/**
+ * Applicational wrapper for the common library class
+ */
 public class AuthServlet extends AuthServletBase {
 
   private static final long serialVersionUID = 7988979181448679156L;
