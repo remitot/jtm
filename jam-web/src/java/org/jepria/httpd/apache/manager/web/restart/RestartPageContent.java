3385c49319ca7080fd39db8e5b6d78ef46715114
remitot <call9096463086@gmail.com>
2019-07-25 15:57:27 +0300

    feat(jam-web/restart): added GUI stub for restart statistics table

--- jam-web/src/java/org/jepria/httpd/apache/manager/web/restart/RestartPageContent.java
+++ jam-web/src/java/org/jepria/httpd/apache/manager/web/restart/RestartPageContent.java
@@ -10,37 +10,47 @@ import org.jepria.web.ssr.Text;
 public class RestartPageContent extends ArrayList<El> {
 
   private static final long serialVersionUID = 2085233077868783633L;
-  
+
   public RestartPageContent(Context context) {
-    
-    // control buttons
-    final ControlButtons controlButtons = new ControlButtons(context);
-    
-    final String restartActionUrl = context.getContextPath() + "/restart";
-    {
-      final Text text = context.getText();
-      
-      final El formRestart = new El("form", context).setAttribute("action", restartActionUrl).setAttribute("method", "post")
-          .addClass("button-form")
-          .addClass("control-button-form_restart");
-      
-      El buttonRestart = new El("button", context)
-          .setAttribute("type", "submit")
-          .addClass("control-button")
-          .addClass("control-button_restart")
-          .addClass("big-black-button")
-          .setInnerHTML(text.getString("org.jepria.httpd.apache.manager.web.restart.ControlButtons.buttonRestart.text"), true);
-          
-      buttonRestart.setAttribute("title", text.getString("org.jepria.httpd.apache.manager.web.restart.ControlButtons.buttonRestart.title"));
-      
-      formRestart.appendChild(buttonRestart);
-      
-      controlButtons.appendChild(formRestart);
+
+    { // control buttons
+      final ControlButtons controlButtons = new ControlButtons(context);
+      controlButtons.addScript("js/restart/restart.js");
+
+      final String restartActionUrl = context.getContextPath() + "/restart";
+      {
+        final Text text = context.getText();
+
+        final El formRestart = new El("form", context).setAttribute("action", restartActionUrl).setAttribute("method", "post")
+            .addClass("button-form")
+            .addClass("control-button-form_restart");
+
+        El buttonRestart = new El("button", context)
+            .setAttribute("type", "submit")
+            .addClass("control-button")
+            .addClass("control-button_restart")
+            .addClass("big-black-button")
+            .setInnerHTML(text.getString("org.jepria.httpd.apache.manager.web.restart.ControlButtons.buttonRestart.text"), true);
+
+        buttonRestart.setAttribute("title", text.getString("org.jepria.httpd.apache.manager.web.restart.ControlButtons.buttonRestart.title"));
+
+        formRestart.appendChild(buttonRestart);
+
+        controlButtons.appendChild(formRestart);
+      }
+
+      add(controlButtons);
     }
     
-    add(controlButtons);
     
-    controlButtons.addScript("js/restart/restart.js");
+    { // restart statistics     // TODO this is a stub
+      El div = new El("div", context);
+      div.setInnerHTML("Здесь можно вывесить табличку со статистикой перезагрузок: дата, время, автор перезагрузки, фактическая длительность и т.д.");
+      div.setAttribute("style", "color: #808080; padding: 15px");
+      add(div);
+    }
+
+
   }
 
 }
