7b042a1bc994998780906b00c5b62e7e27c7cc3b
roma <you@gmail.com>
2019-09-29 00:05:37 +0300

    feat(web): handle and print exceptions on Sources pages

--- jam-web/src/java/org/jepria/httpd/apache/manager/web/sources/SourcesServlet.java
+++ jam-web/src/java/org/jepria/httpd/apache/manager/web/sources/SourcesServlet.java
@@ -1,16 +1,19 @@
 package org.jepria.httpd.apache.manager.web.sources;
 
-import org.jepria.httpd.apache.manager.web.Environment;
-import org.jepria.httpd.apache.manager.web.EnvironmentFactory;
-import org.jepria.web.ssr.Context;
-import org.jepria.web.ssr.El;
-import org.jepria.web.ssr.HtmlPageBaseBuilder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.jepria.httpd.apache.manager.web.Environment;
+import org.jepria.httpd.apache.manager.web.EnvironmentFactory;
+import org.jepria.web.ssr.Context;
+import org.jepria.web.ssr.El;
+import org.jepria.web.ssr.HtmlPageBaseBuilder;
 
 public class SourcesServlet extends HttpServlet {
 
@@ -25,9 +28,37 @@ public class SourcesServlet extends HttpServlet {
     pageBuilder.setTitle(context.getText().getString("org.jepria.httpd.apache.manager.web.sources.title"));
 
     StringBuilder preContent = new StringBuilder();
-    preContent.append("mod_jk configuration:    ").append(env.getMod_jk_confFile().toString()).append("\n");
-    preContent.append("workers properties:      ").append(env.getWorkers_propertiesFile().toString()).append("\n");
-    preContent.append("Apache service name:     ").append(env.getApacheServiceName()).append("\n");
+    
+    String modJkSourceValue = null;
+    {
+      try { // no throwable must be thrown on this page
+        modJkSourceValue = env.getMod_jk_confFile().toString();
+      } catch (Throwable e) {
+        modJkSourceValue = stackTraceAsValue(e);
+      }
+    }
+    
+    String workersPropertiesSourceValue = null;
+    {
+      try { // no throwable must be thrown on this page
+        workersPropertiesSourceValue = env.getWorkers_propertiesFile().toString();
+      } catch (Throwable e) {
+        workersPropertiesSourceValue = stackTraceAsValue(e);
+      }
+    }
+    
+    String apacheServiceNameSourceValue = null;
+    {
+      try { // no throwable must be thrown on this page
+        apacheServiceNameSourceValue = env.getApacheServiceName();
+      } catch (Throwable e) {
+        apacheServiceNameSourceValue = stackTraceAsValue(e);
+      }
+    }
+    
+    preContent.append("mod_jk configuration:    ").append(modJkSourceValue).append("\n\n");
+    preContent.append("workers properties:      ").append(workersPropertiesSourceValue).append("\n\n");
+    preContent.append("Apache service name:     ").append(apacheServiceNameSourceValue).append("\n\n");
 
     El pre = new El("pre", context);
     pre.setInnerHTML(preContent.toString(), true);
@@ -35,6 +66,23 @@ public class SourcesServlet extends HttpServlet {
     pageBuilder.getBody().appendChild(pre);
 
     pageBuilder.build().respond(resp);
+  }
+  
+  /**
+   * Pretty-prints stacktrace of the Throwable as a value of corresponding source
+   * @param e
+   * @return
+   */
+  protected String stackTraceAsValue(Throwable e) {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    e.printStackTrace(new PrintStream(baos));
+    String stackTrace = baos.toString();
+    if (stackTrace != null && stackTrace.endsWith("\n")) {
+      stackTrace = stackTrace.substring(0, stackTrace.length() - 1);
+    }
+    String ret = "<Failed to get value, stacktrace below>\n" + 
+        "                         " + stackTrace.replaceAll("\n\t", "\n" + "                         " + "\t");
+    return ret;
   }
 
 }
--- jtm-web/src/java/org/jepria/tomcat/manager/web/sources/SourcesServlet.java
+++ jtm-web/src/java/org/jepria/tomcat/manager/web/sources/SourcesServlet.java
@@ -1,16 +1,19 @@
 package org.jepria.tomcat.manager.web.sources;
 
-import org.jepria.tomcat.manager.web.Environment;
-import org.jepria.tomcat.manager.web.EnvironmentFactory;
-import org.jepria.web.ssr.Context;
-import org.jepria.web.ssr.El;
-import org.jepria.web.ssr.HtmlPageBaseBuilder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.jepria.tomcat.manager.web.Environment;
+import org.jepria.tomcat.manager.web.EnvironmentFactory;
+import org.jepria.web.ssr.Context;
+import org.jepria.web.ssr.El;
+import org.jepria.web.ssr.HtmlPageBaseBuilder;
 
 public class SourcesServlet extends HttpServlet {
 
@@ -25,9 +28,37 @@ public class SourcesServlet extends HttpServlet {
     pageBuilder.setTitle(context.getText().getString("org.jepria.tomcat.manager.web.sources.title"));
 
     StringBuilder preContent = new StringBuilder();
-    preContent.append("Context configuration:   ").append(env.getContextXml().toString()).append("\n");
-    preContent.append("Server configuration:    ").append(env.getServerXml().toString()).append("\n");
-    preContent.append("Logs directory:          ").append(env.getLogsDirectory().toString()).append("\n");
+    
+    String contextXmlSourceValue = null;
+    {
+      try { // no throwable must be thrown on this page
+        contextXmlSourceValue = env.getContextXml().toString();
+      } catch (Throwable e) {
+        contextXmlSourceValue = stackTraceAsValue(e);
+      }
+    }
+    
+    String serverXmlSourceValue = null;
+    {
+      try { // no throwable must be thrown on this page
+        serverXmlSourceValue = env.getServerXml().toString();
+      } catch (Throwable e) {
+        serverXmlSourceValue = stackTraceAsValue(e);
+      }
+    }
+    
+    String logsDirectorySourceValue = null;
+    {
+      try { // no throwable must be thrown on this page
+        logsDirectorySourceValue = env.getLogsDirectory().toString();
+      } catch (Throwable e) {
+        logsDirectorySourceValue = stackTraceAsValue(e);
+      }
+    }
+    
+    preContent.append("Context configuration:   ").append(contextXmlSourceValue).append("\n");
+    preContent.append("Server configuration:    ").append(serverXmlSourceValue).append("\n");
+    preContent.append("Logs directory:          ").append(logsDirectorySourceValue).append("\n");
 
     El pre = new El("pre", context);
     pre.setInnerHTML(preContent.toString(), true);
@@ -35,6 +66,23 @@ public class SourcesServlet extends HttpServlet {
     pageBuilder.getBody().appendChild(pre);
 
     pageBuilder.build().respond(resp);
+  }
+  
+  /**
+   * Pretty-prints stacktrace of the Throwable as a value of corresponding source
+   * @param e
+   * @return
+   */
+  protected String stackTraceAsValue(Throwable e) {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    e.printStackTrace(new PrintStream(baos));
+    String stackTrace = baos.toString();
+    if (stackTrace != null && stackTrace.endsWith("\n")) {
+      stackTrace = stackTrace.substring(0, stackTrace.length() - 1);
+    }
+    String ret = "<Failed to get value, stacktrace below>\n" + 
+        "                         " + stackTrace.replaceAll("\n\t", "\n" + "                         " + "\t");
+    return ret;
   }
 
 }
